e71ce28ec73d729e5536666d6b0efaacc98161e9
AndreQuintero <andre.quintero96@gmail.com>
2025-05-18 01:11:59 +0800

    feat: add with-custom-cursor HOC for custom cursor behavior
    Introduce a higher-order component (HOC) that enables custom cursor behavior. The HOC wraps any component and adds a customizable cursor that follows mouse movements. This enhances user interaction by providing a more dynamic and engaging UI experience.

---
+++ src/with-custom-cursor.tsx
@@ -0,0 +1,51 @@
+import { useRef, type ComponentType, type ForwardRefExoticComponent, type MouseEvent, type RefAttributes } from "react"
+import styles from "./styles.module.css"
+
+export type WithCustomCursorProps = {
+    onMouseMove: (e: MouseEvent<HTMLDivElement>) => void
+    CursorComponent:  ForwardRefExoticComponent<React.HTMLAttributes<HTMLElement> & RefAttributes<HTMLElement>>
+}
+
+export const WithCustomCursor = <P extends WithCustomCursorProps>(
+    WrappedComponent: ComponentType<P>
+  ) => {
+    return function CustomCursor({ CursorComponent, ...props }: WithCustomCursorProps) {
+      const cursor = useRef<HTMLDivElement>(null)
+  
+      const mouseMove = (e: MouseEvent<HTMLDivElement>) => {
+        const rect = e.currentTarget.getBoundingClientRect()
+        const axisX = e.clientX - rect.left
+        const axisY = e.clientY - rect.top
+        if (cursor && cursor.current) {
+          const { x, y } = getCenterOfCursor(
+            cursor.current.clientWidth,
+            cursor.current.clientHeight,
+            axisX,
+            axisY
+          )
+          cursor.current.style.transform = `translate3d(${x}px, ${y}px, 0)`
+        }
+      }
+  
+      const getCenterOfCursor = (
+        widthElement: number,
+        heightElement: number,
+        xAxis: number,
+        yAxis: number
+      ) => {
+        const x = xAxis - widthElement / 2
+        const y = yAxis - heightElement / 2
+  
+        return { x, y }
+      }
+  
+      return (
+        <div className={styles.wrapper}>
+            <div className={styles.cursor}>
+                <CursorComponent ref={cursor} />
+            </div>
+            <WrappedComponent {...(props as P)} onMouseMove={mouseMove} />
+        </div>
+      )
+    }
+  }
